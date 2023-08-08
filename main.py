03c75b709de403839a122440f898990db641fd3d
antonp <grfokpr@gmail.com>
2023-08-08 21:27:48 +0300

    fix

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+from decouple import config
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Float, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import uvicorn
+
+DATABASE_URL = config('DATABASE_URL')
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://myappuser:password@localhost/myappdb"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 class Number(Base):
     __tablename__ = "numbers"
@@ -20,11 +26,14 @@ class Number(Base):
     num2 = Column(Float)
     result = Column(Float)
 
+
 Base.metadata.create_all(bind=engine)
+
 
 class Numbers(BaseModel):
     num1: float
     num2: float
+
 
 @app.post("/add")
 def add_numbers(numbers: Numbers):
@@ -38,3 +47,7 @@ def add_numbers(numbers: Numbers):
     db.close()
 
     return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
